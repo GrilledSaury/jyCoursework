4e7567dbee9c2d481c57a02c2596b5d497b5ec5e
Layzzi <33226552+SiuGaaLei@users.noreply.github.com>
2022-10-12 02:14:22 +0800

    view

---
+++ src/components/CourseEditor.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { watch } from 'vue'
+import { draft } from '../state.js'
+import { Calendar, DatePicker } from 'v-calendar'
+import 'v-calendar/dist/style.css'
+const props = defineProps(['modelValue'])
+const emits = defineEmits(['update:modelValue'])
+
+</script>
+
+<template>
+  <Transition name="fade">
+    <div class="fixed top-0 left-0 w-screen h-screen bg-black flex items-center justify-center" v-if="props.modelValue" style="background-color: rgba(0, 0, 0, 0.5);" @click="emits('update:modelValue', false)">
+      <div class="fixed relative px-10 py-8 bg-white rounded flex flex-col justify-center items-center" @click.stop="">
+        <div class="flex items-center">
+          <div class="text-gray-500">Course Name</div>
+          <input v-model="draft.name" class="all-transition border rounded m-2 px-2 focus:border-blue-500">
+        </div>
+        <div class="flex items-center">
+          <div class="text-gray-500">Course Code</div>
+          <input v-model="draft.code" class="all-transition border rounded m-2 px-2 focus:border-blue-500 font-mono">
+        </div>
+        <div class="flex items-center">
+          <div class="text-gray-500 m-2">Language</div>
+          <input class="m-1" type="radio" id="Chinese" value="Chinese" v-model="draft.lang">
+          <label class="m-1" for="Chinese">Chinese</label>
+          <input class="m-1" type="radio" id="English" value="English" v-model="draft.lang">
+          <label class="m-1" for="English">English</label>
+          <input class="m-1" type="radio" id="Bilingual" value="Bilingual" v-model="draft.lang">
+          <label class="m-1" for="Bilingual">Bilingual</label>
+        </div>
+        <div class="flex items-center">
+          <div class="text-gray-500">Teacher</div>
+          <input v-model="draft.teacher" class="all-transition border rounded m-2 px-2 focus:border-blue-500">
+        </div>
+        <div class="flex items-center">
+          <div class="text-gray-500">Date&Time</div>
+          <DatePicker class="ml-2" v-model="draft.date" mode="dateTime" is24hr :model-config="{ type: 'number' }">
+            <template v-slot="{ inputValue, inputEvents }">
+              <input
+                class="px-2 py-1 border rounded focus:outline-none focus:border-blue-300"
+                :value="inputValue"
+                v-on="inputEvents"
+              />
+            </template>
+          </DatePicker>
+        </div>
+        <div class="flex items-center">
+          <div class="text-gray-500">Location</div>
+          <select v-model="draft.location" class="m-2 text-center border rounded hover:border-blue-500 all-transition">
+            <option value="Teaching Building No.1 Lecture Hall" selected>Teaching Building No.1 Lecture Hall</option>
+            <option value="Research Building Lecture Hall">Research Building Lecture Hall</option>
+            <option value="Library Conference Hall and Activity Room">Library Conference Hall and Activity Room</option>
+          </select>
+        </div>
+        <div class="flex items-center">
+          <div class="text-gray-500">Duration</div>
+          <div class="font-mono">/hour(s)</div>
+          <input v-model="draft.duration" type=number class="all-transition border rounded m-2 px-2 focus:border-blue-500 w-12">
+        </div>
+        <div class="flex items-center justify-center">
+          <button class="all-transition px-2 text-blue-500 font-bold rounded m-1 border border-blue-500 hover:text-white hover:bg-blue-500">submit</button>
+          <button class="all-transition px-2 text-red-500 font-bold rounded m-1 border border-red-500 hover:text-white hover:bg-red-500" v-if="!draft.new">delete</button>
+          <button class="all-transition px-2 text-yellow-500 font-bold rounded m-1 border border-yellow-500 hover:text-white hover:bg-yellow-500" @click="emits('update:modelValue', false)">cancel</button>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,79 @@
 <script setup>
-let cot = $ref(0)
+import { PlusIcon } from '@heroicons/vue/24/solid'
+import CourseEditor from '../components/CourseEditor.vue'
+import { draft } from '../state.js'
+import moment from 'moment'
 
-function inc () {
-  cot++
+let courses = $ref({})
+let show = $ref(false)
+
+const get = async () => ({
+  testid: {
+    name: 'Sample',
+    code: 'SP000',
+    lang: 'English',
+    teacher: 'Jingyuan Zhang',
+    date: 1665497437398,
+    location: 'Research Building Lecture Hall',
+    duration: 3
+  }
+})
+
+async function init () {
+  courses = await get()
 }
+
+init()
+
+function updateDraft (o) {
+  if (o.new) {
+    for (const k in draft) delete draft[k]
+    draft.new = true
+    return
+  }
+  draft.new = false
+  for (const k in o) draft[k] = o[k]
+}
+
 </script>
 
 <template>
-  <h1>Home</h1>
-  <p>cot: {{ cot }}</p>
-  <button @click="inc">Click Me</button>
+  <div class="px-10 py-8 bg-gray-100 min-h-screen flex flex-col items-center">
+    <div class="text-4xl font-bold m-8">Open Courses</div>
+    <div class="bg-white rounded-md p-4">
+      <table class="table-auto text-center">
+        <thead class="text-gray-600 border-b">
+          <tr>
+            <th class="py-1 px-2 text-xl border-r">Course Name</th>
+            <th class="py-1 px-2 text-xl border-r">Course Code</th>
+            <th class="py-1 px-2 text-xl border-r">Language</th>
+            <th class="py-1 px-2 text-xl border-r">Teacher</th>
+            <th class="py-1 px-2 text-xl border-r">Date</th>
+            <th class="py-1 px-2 text-xl border-r">Time</th>
+            <th class="py-1 px-2 text-xl border-r">Location</th>
+            <th class="py-1 px-2 text-xl">Duration</th>
+          </tr>
+        </thead>
+        <tbody class="bg-gray-100">
+          <tr v-for="c in courses" class="border-b all-transition hover:bg-blue-100 cursor-pointer" @click="updateDraft(c); show = true">
+            <td class="py-1 px-2 border-r">{{ c.name }}</td>
+            <td class="py-1 px-2 border-r font-mono">{{ c.code }}</td>
+            <td class="py-1 px-2 border-r">{{ c.lang }}</td>
+            <td class="py-1 px-2 border-r">{{ c.teacher }}</td>
+            <td class="py-1 px-2 border-r font-mono">{{ moment(c.date).format('YYYY/MM/DD') }}</td>
+            <td class="py-1 px-2 border-r font-mono">{{ moment(c.date).format('HH:mm') }}</td>
+            <td class="py-1 px-2 border-r">{{ c.location }}</td>
+            <td class="py-1 px-2 font-mono">{{ c.duration }}h</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="flex items-center justify-center m-2">
+        <button class="flex items-center all-transition bg-green-500 font-bold text-white rounded-full shadow hover:shadow-md mx-3 my-1 px-2 py-1 w-32" @click="updateDraft({ new: true }); show = true">
+          <PlusIcon class="w-5 m-1" />
+          Add Class
+        </button>
+      </div>
+    </div>
+    <CourseEditor v-model="show" />
+  </div>
 </template>
